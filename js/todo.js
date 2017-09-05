153194d8c99058d51542e164bd42ee5e46915259
mthorry <mthorry@gmail.com>
2017-09-05 16:36:14 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm()
 });
 
-// define functions here
+
+function submitForm() {
+  $('form').on('submit', function(event){
+  var input = $('#item').val();
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(input);
+  node.appendChild(textnode);
+
+  document.getElementById("list").children[0].appendChild(node);
+  // document.getElementById("list").children[0].innerHTML += `<li>${input}</li>`
+  // tests only passed with appendChild
+
+  event.preventDefault();
+});
+}
+
+
